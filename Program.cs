6925076430a8cc4ec4931d3d79346c690d31ebdc
Vlad M <vlad.mackutchev@gmail.com>
2022-05-12 21:16:24 +0300

    task2

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿Console.Clear();
+
+Console.WriteLine("Введи высоту массива:");
+int m = int.Parse(Console.ReadLine());
+Console.WriteLine("Введи ширину массива:");
+int n = int.Parse(Console.ReadLine());
+
+int[,] numbers = GetArray(m, n);
+PrintArray(numbers);
+
+
+int[,] GetArray(int rows, int columns)
+{
+    int[,] result = new int[rows, columns];
+
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            result[i, j] = i + j;
+        }
+
+    }
+    return result;
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]}  ");
+        }
+        Console.WriteLine();
+    }
+
+}
